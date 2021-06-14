aee2f120dc8a53b4ed4308de9eb73062c26ffd8c
Antonin Rey Millet <antoninreymillet@gmail.com>
2021-06-14 16:30:47 +0200

    Mods deploiements

--- src/components/About.js
+++ src/components/About.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import sanityClient from "../client";
-import backgroundAuthorImage from "../Images/Wallpaper_Home.jpg";
+import backgroundAuthor from "../Images/Wallpaper_Home.jpg";
 import imageUrlBuilder from "@sanity/image-url";
 import BlockContent from "@sanity/block-content-to-react";
 
@@ -26,7 +26,7 @@ export default function About() {
 
     return (
         <main className="relative">
-            <img src={backgroundAuthorImage} alt="Background Author Image" className="absolute w-full"/>
+            <img src={backgroundAuthor} className="absolute w-full"/>
             <div className="p-10 lg:pt-48 container mx-auto relative">
                 <section className="bg-green-800 rounded-lg shadow-2xl lg:flex p-20">
                     <img src={urlFor(author.authorImage).url()} className="rounded w-32 h-32 lg:w-64 lg:h-64 mr-8" alt={author.name}/>
